6ed2f6e63ebfa2ecbde43327eaa761ee698e115b
Guilherme Zini <guilhermerosazini@gmail.com>
2024-05-07 21:08:45 -0300

    Aula Herança 01

---
+++ HerancaExemplo/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaExemplo.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+        
+        public Account() //Construtores
+        { 
+        }
+        public Account (int number, string holder, double balance) //Construtores com argumentos
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw (double amount) //Quantia (amount) é retirado do saque da conta (balance)
+        {
+            Balance = amount;
+        }
+        
+        public void Deposit(double amount) //Pegar saldo e acrescentar a quantia
+        { 
+            Balance += amount;
+        }
+    }
+}
---
+++ HerancaExemplo/Entities/BusinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaExemplo.Entities
+{
+    internal class BusinessAccount : Account // (: Account) Para extender a classe account à BusinessAccount
+    {
+        public double LoanLimite { get; set; }
+
+        public BusinessAccount() //Vazio para ter opção de criar objeto sem passar dados
+        {
+        }
+
+        public BusinessAccount (int number, string holder, double balance, double loanLimite) 
+            : base(number, holder, balance) //(:base) aproveia o contrutor da classe account
+        {
+            LoanLimite = loanLimite;
+        }
+
+        public void Loan (double amount) 
+        {
+            if (amount <= LoanLimite)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
